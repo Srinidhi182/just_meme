108d98033253630bb4714e6190a9e061eb6c123e
Srinidhi.R <20010114475.SRINIDHI@cse.reva.edu.in>
2023-12-03 19:08:26 +0530

    first commit

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+//---Import Icon
+import { MdNotifications } from "react-icons/md";
+import { BsSearch } from "react-icons/bs";
+import { CgMenuLeft, CgMenuRight } from "react-icons/cg";
+// import image from "../img";
+
+//--Internal import
+import Style from "./Navbar.module.css";
+import { Discover, HelpCenter, Profile, Notification, SideBar } from "./index";
+import { Button } from "../componentindex";
+import image from "../img/index";
+
+const Navbar = () => {
+  //---USESTATE COMPONENTS
+  const [discover, setDiscover] = useState(false);
+  const [help, setHelp] = useState(false);
+  const [notification, setNotification] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const [openSideMenu, setOpenSideMenu] = useState(false);
+
+  const openMenu = (e) => {
+    const btnText = e.target.innerText;
+    if (btnText == "Discover") {
+      setDiscover(true);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    } else if (btnText == "HelpCenter") {
+      setDiscover(false);
+      setHelp(true);
+      setNotification(false);
+      setProfile(false);
+    } else {
+      setDiscover(false);
+      setHelp(false);
+      setNotification(false);
+      setProfile(false);
+    }
+  };
+
+  const openNotification = () => {
+    if (!notification) {
+      setNotification(true);
+      setDiscover(false);
+      setHelp(false);
+      setProfile(false);
+    } else {
+      setNotification(false);
+    }
+  };
+
+  const openProfile = () => {
+    if (!profile) {
+      setProfile(true);
+      setHelp(false);
+      setDiscover(false);
+      setNotification(false);
+    } else {
+      setProfile(false);
+    }
+  };
+
+  const openSideBar = () => {
+    if (!openSideMenu) {
+      setOpenSideMenu(true);
+    } else {
+      setOpenSideMenu(false);
+    }
+  };
+
+  return (
+    <div className={Style.navbar}>
+      <div className={Style.navbar_container}>
+        <div className={Style.navbar_container_left}>
+          <div className={Style.logo}>
+            <Image
+              src={image.logo}
+              alt="Image not found..."
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={Style.navbar_container_left_box_input}>
+            <div className={Style.navbar_container_left_box_input_box}>
+              <input type="text" placeholder="Search NFT" />
+              <BsSearch onClick={() => {}} className={Style.search_icon} />
+            </div>
+          </div>
+        </div>
+        {/* end of left section  */}
+
+        <div className={Style.navbar_container_right}>
+          <div className={Style.navbar_container_right_discover}>
+            {/* Discover Menu*/}
+            <p onClick={(e) => openMenu(e)}>Discover</p>
+            {discover && (
+              <div className={Style.navbar_container_right_discover_box}>
+                <Discover />
+              </div>
+            )}
+          </div>
+
+          {/* HelpCenter */}
+          <div className={Style.navbar_container_right_help}>
+            <p onClick={(e) => openMenu(e)}>HelpCenter</p>
+            {help && (
+              <div className={Style.navbar_container_right_help_box}>
+                <HelpCenter />
+              </div>
+            )}
+          </div>
+
+          {/* Notification */}
+          <div className={Style.navbar_container_right_notify}>
+            <MdNotifications
+              className={Style.notify}
+              onClick={() => openNotification()}
+            />
+            {notification && <Notification />}
+          </div>
+
+          {/* create button section */}
+          <div className={Style.navbar_container_right_button}>
+            <Button btnName="Create" handleClick={() => {}} />
+          </div>
+
+          {/* user profile */}
+          <div className={Style.navbar_container_right_profile_box}>
+            <div className={Style.navbar_container_right_profile}>
+              <Image
+                src={image.user1}
+                alt="Profile"
+                width={40}
+                height={40}
+                onClick={() => openProfile()}
+                className={Style.navbar_container_right_profile}
+              />
+              {profile && <Profile />}
+            </div>
+          </div>
+
+          {/* menu button */}
+          <div className={Style.navbar_container_right_menubtn}>
+            <CgMenuRight
+              className={Style.menuIcon}
+              onClick={() => openSideBar()}
+            />
+          </div>
+        </div>
+      </div>
+      {/* Sidebar - this will be displayed only in the mobile device */}
+      {openSideMenu && (
+        <div className={Style.SideBar}>
+          <SideBar setOpenSideMenu={setOpenSideMenu} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ components/img/index.js
@@ -0,0 +1,75 @@
+import background1 from "./background1.png";
+import background2 from "./background2.png";
+import background3 from "./background3.png";
+import bg4 from "./bg4.png";
+import bg5 from "./bg5.png";
+import eran from "./eran.png";
+import hero from "./hero.png";
+import musicwave from "./musicwave.png";
+import nft_1 from "./nft_1.png";
+import nftimage1 from "./nftimage1.png";
+import nftimae2 from "./nftimage2.png";
+import nftimage3 from "./nftimage3.png";
+import pg6 from "./pg6.png";
+import pg7 from "./pg7.png";
+import pg8 from "./pg8.png";
+import pg9 from "./pg9.png";
+import service1 from "./service1.png";
+import service2 from "./service2.png";
+import service3 from "./service3.png";
+import service4 from "./service4.png";
+import update from "./update.png";
+import user1 from "./user1.png";
+import user2 from "./user2.png";
+import user3 from "./user3.png";
+import user4 from "./user4.png";
+import user5 from "./user5.png";
+import user6 from "./user6.png";
+import user8 from "./user8.png";
+import user9 from "./user9.png";
+import user10 from "./user10.png";
+import facebook from "./facebook.svg";
+import telegram from "./telegram.svg";
+import twitter from "./twitter.svg";
+import vimeo from "./vimeo.svg";
+import youtube from "./youtube.svg";
+import logo from "./logo.png";
+
+export default {
+  logo,
+  update,
+  musicwave,
+  nftimage1,
+  nftimae2,
+  nftimage3,
+  nft_1,
+  background1,
+  background2,
+  background3,
+  bg4,
+  bg5,
+  eran,
+  hero,
+  pg6,
+  pg7,
+  pg8,
+  pg9,
+  service1,
+  service2,
+  service3,
+  service4,
+  user1,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  user8,
+  user9,
+  user10,
+  facebook,
+  telegram,
+  twitter,
+  vimeo,
+  youtube,
+};
